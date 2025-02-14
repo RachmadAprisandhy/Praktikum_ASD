c8176397b00ee71fee4caabab295e5b031406260
Rachmad Aprisandy <rachmadblackspade212@gmail.com>
2025-02-14 15:23:15 +0700

    commit 3

--- percobaan2.java
+++ percobaan2.java
@@ -4,14 +4,12 @@ public class percobaan2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;
+        System.out.print("masukkan angka anda = ");
         n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            if (i == 6 || i ==10){
-                System.out.print("* ");
-            }
-            if (i % 2 != 0) { 
-                System.out.print("* ");
+            if (i % 2 != 0 || i==6 || i==10) { 
+                System.out.print("*  ");
             } else {
                 System.out.print(i + " ");
             }
