f4133c2c659cbfd952acdbc815b0361c831502c1
RachmadAprisandhy <124744940+RachmadAprisandhy@users.noreply.github.com>
2025-02-21 09:16:40 +0700

    Add files via upload

---
+++ jobsheet2/mahasiswa18.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class mahasiswa18 {
+
+    String nama;
+    String NIM;
+    String Kelas;
+    double IPK;
+
+    void TampilkanInformasi(){
+        System.out.println("Nama :"+ nama);
+        System.out.println("NIM  :"+ NIM);
+        System.out.println("Kelas:"+ Kelas);
+        System.out.println("IPK  :"+ nama);
+    }
+    void ubahkelas(String KelasBaru){
+        Kelas = KelasBaru;
+    }
+    void updateIPK(double IPKBaru){
+        if (IPKBaru < 0.0 || IPKBaru > 4.0){
+            System.out.println("IPK tidak valid harus di rentang 0.0 - 4.0");
+        }else{
+            IPK = IPKBaru;
+        }
+    }
+    String nilaiKerja(){
+        if (IPK >= 3.5){
+            return "Kinerja Sangat Baik";
+        }else if(IPK >= 3.0){
+            return "Kinerja Baik";
+        }else if (IPK >= 2.0){
+            return "Kinerja Cukup";
+        }else{
+            return"kinerja Kurang";
+        }
+    }
+}
