f7762a39b929c613c4f6cc2ef661787a705286f3
RachmadAprisandhy <124744940+RachmadAprisandhy@users.noreply.github.com>
2025-02-28 21:35:04 +0700

    Add files via upload

--- Jobsheet3/MataKuliah18.java
+++ Jobsheet3/MataKuliah18.java
@@ -1,13 +1,32 @@
+import java.util.Scanner;
+
 public class MataKuliah18{
     public String Kode;
     public String Nama;
     public int sks;
     public int JumlahJam;
 
-    public MataKuliah18(String Kode, String Nama,int sks, int JumlahJam){
-        this.Kode = Kode;
-        this.Nama = Nama;
-        this.sks = sks;
-        this.JumlahJam = JumlahJam;
+
+   public MataKuliah18(){
+    }
+    
+    public void TampilkanInformasi() {
+        System.out.println("Kode       : " + Kode);
+        System.out.println("Nama       : " + Nama);
+        System.out.println("SKS        : " + sks);
+        System.out.println("Jumlah Jam : " + JumlahJam);
+        System.out.println("----------------------------------------");
+    }
+    public void tambahData(Scanner sc) {
+        System.out.print("Kode       : ");
+        this.Kode = sc.nextLine();
+        System.out.print("Nama       : ");
+        this.Nama = sc.nextLine();
+        System.out.print("SKS        : ");
+        this.sks = sc.nextInt();
+        System.out.print("Jumlah Jam : ");
+        this.JumlahJam = sc.nextInt();
+        sc.nextLine(); 
+        System.out.println("----------------------------------------");
     }
 }
