28dd1efa38d891a6a3f34c08c993ff3019ea162d
RachmadAprisandhy <124744940+RachmadAprisandhy@users.noreply.github.com>
2025-02-28 21:36:23 +0700

    Add files via upload

--- Jobsheet3/MataKuliahDemo18.java
+++ Jobsheet3/MataKuliahDemo18.java
@@ -1,36 +1,31 @@
 import java.util.Scanner;
-public class MataKuliahDemo18{
+
+public class MataKuliahDemo18 {
     public static void main(String[] args) {
         Scanner sc18 = new Scanner(System.in);
-        MataKuliah18 [] arrayOfMataKuliah = new MataKuliah18[3];
-        String Kode,Nama,dummy;
-        int sks,JumlahJam;
 
-        for (int i = 0; i < 3;i++){
-            System.out.println("Masukkan Data Matakuliah ke-" + (i +1));
-            System.out.print("Kode      :");
-            Kode = sc18.nextLine();
-            System.out.print("nama      :");
-            Nama = sc18.nextLine();
-            System.out.print("SKS      :");
-            dummy = sc18.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("JumlahJam      :");
-            dummy = sc18.nextLine();
-            JumlahJam = Integer.parseInt(dummy);
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMataKuliah = sc18.nextInt();
+        sc18.nextLine(); 
+
+        MataKuliah18[] arrayOfMataKuliah = new MataKuliah18[jumlahMataKuliah];
+
+    
+        for (int i = 0; i < jumlahMataKuliah; i++) {
+            System.out.println("Masukkan Data Mata Kuliah ke-" + (i + 1));
+            arrayOfMataKuliah[i] = new MataKuliah18(); 
+            arrayOfMataKuliah[i].tambahData(sc18); 
+        }
+        for (int i = 0; i < jumlahMataKuliah; i++) {
+            System.out.println("Data Mata Kuliah ke-" + (i + 1));
+            System.out.println("Kode       : " + arrayOfMataKuliah[i].Kode);
+            System.out.println("Nama       : " + arrayOfMataKuliah[i].Nama);
+            System.out.println("SKS        : " + arrayOfMataKuliah[i].sks);
+            System.out.println("Jumlah Jam : " + arrayOfMataKuliah[i].JumlahJam);
             System.out.println("----------------------------------------");
-
-            arrayOfMataKuliah[i] = new MataKuliah18(Kode, Nama, sks, JumlahJam);
-        }
-        for (int i = 0 ; i < 3; i++){
-            System.out.println("Data Mata kuliah ke-" + (i + 1));
-            System.out.println("Kode        : " + arrayOfMataKuliah[i].Kode);
-            System.out.println("Nama        : " + arrayOfMataKuliah[i].Nama);
-            System.out.println("SKS         : " + arrayOfMataKuliah[i].sks);
-            System.out.println("Jumlah Jam  : " + arrayOfMataKuliah[i].JumlahJam);
-            System.out.println("----------------------------------------------------");
         }
 
-        sc18.close();
+        sc18.close(); 
     }
 }
+
