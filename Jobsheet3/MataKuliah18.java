759aa21624b9116ef0aa987f69ce6bc68c0b065a
RachmadAprisandhy <124744940+RachmadAprisandhy@users.noreply.github.com>
2025-02-28 20:01:41 +0700

    Add files via upload

---
+++ Jobsheet3/MataKuliah18.java
@@ -0,0 +1,13 @@
+public class MataKuliah18{
+    public String Kode;
+    public String Nama;
+    public int sks;
+    public int JumlahJam;
+
+    public MataKuliah18(String Kode, String Nama,int sks, int JumlahJam){
+        this.Kode = Kode;
+        this.Nama = Nama;
+        this.sks = sks;
+        this.JumlahJam = JumlahJam;
+    }
+}
