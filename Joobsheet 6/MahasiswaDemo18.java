d3ecd3d2c1fc4b11945024cceebceb3e67a022c4
RachmadAprisandhy <124744940+RachmadAprisandhy@users.noreply.github.com>
2025-03-28 20:38:35 +0700

    Add files via upload

--- Joobsheet 6/MahasiswaDemo18.java
+++ Joobsheet 6/MahasiswaDemo18.java
@@ -1,24 +1,44 @@
-public class MahasiswaDemo18{
+import java.util.Scanner;
 
+public class MahasiswaDemo18 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         MahasiswaBerprestasi18 list = new MahasiswaBerprestasi18();
-        Mahasiswa18 m1 = new Mahasiswa18( "123", "Zidan", "2A", 3.2);
-        Mahasiswa18 m2 = new Mahasiswa18( "124", "Ayu", "2A", 3.5);
-        Mahasiswa18 m3 = new Mahasiswa18( "125", "Sofi", "2A", 3.1);
-        Mahasiswa18 m4 = new Mahasiswa18( "126", "Sita", "2A", 3.9);
-        Mahasiswa18 m5 = new Mahasiswa18( "127", "miki", "2A", 3.7);
 
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
+        System.out.print("Masukkan Jumlah Mahasiswa: ");
+        int jmlmhs = input.nextInt();
+        input.nextLine(); 
 
-        System.out.println("Data mahasiswa Sebelum sorting : ");
+        for (int i = 0; i < jmlmhs; i++) {
+            System.out.println("Masukkan Data mahasiswa ke - " + (i + 1));
+
+            System.out.print("NIM : ");
+            String nim = input.nextLine();
+
+            System.out.print("Nama : ");
+            String nama = input.nextLine();
+
+            System.out.print("Kelas : ");
+            String kelas = input.nextLine();
+
+            System.out.print("IPK : ");
+            double ipk = input.nextDouble();
+            input.nextLine(); 
+
+
+            Mahasiswa18 MHA = new Mahasiswa18(nim, nama, kelas, ipk);
+            list.tambah(MHA);
+        }
+
+
+        System.out.println("Data mahasiswa sebelum sorting:");
         list.tampil();
 
-        System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
+  
         list.bubblesort();
+
+
+        System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC):");
         list.tampil();
     }
 }
