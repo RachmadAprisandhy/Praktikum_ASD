599872775ee950771addf0f244732415006cc617
RachmadAprisandhy <124744940+RachmadAprisandhy@users.noreply.github.com>
2025-05-23 15:07:20 +0700

    Add files via upload

---
+++ JOBSHEET 11/Mahasiswa18.java
@@ -0,0 +1,15 @@
+public  class Mahasiswa18 {
+    String NIM,nama ,kelas;
+    double ipk;
+
+    Mahasiswa18(String NIM, String nama, String kelas, double ipk){
+        this.NIM = NIM;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+
+    public void tampilInformasi (){
+        
+    }
+}
